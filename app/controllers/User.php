eddb13bfc4ea2af1f1986d6501506d6c139f3344
CalvinCandra <willylay12@gmail.com>
2021-11-03 12:23:13 +0800

    view and asset

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -1,12 +1,21 @@
 <?php
 
-    class User{
-        public function index() {
-            echo "User/index";
-        }
-        public function profile($nama = "Linux", $pekerjaan = "Devs") {
-            echo "Salam kenal saya $nama, saya seorang $pekerjaan";
-        }
+class User extends Controller {
+    public function index() {
+      $data["judul"]='Index';
+      $this->view("templates/header", $data);
+      $this->view("user/index", $data);
+      $this->view("templates/footer");
+      
     }
+    public function profile($nama = "Linux", $pekerjaan = "Devs") {
+      $data["judul"]= 'User';
+      $data["nama"] = $nama;
+      $data["pekerjaan"] = $pekerjaan;
+      $this->view("templates/header", $data);
+      $this->view("user/profile", $data);
+      $this->view("templates/footer");
+    }
+  }
 
 ?>
---
+++ app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php
+define('BASE_URL', "http://localhost/mvc_pwpb/public");
+
+?>
